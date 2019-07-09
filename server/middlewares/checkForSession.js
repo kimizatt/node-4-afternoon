057b04eadaac0bd46038666c987eb320258d9faf
Kimberly Izatt <kimizatt@Kims-MacBook-Pro.local>
2019-07-09 16:38:22 -0600

    Done with project

---
+++ server/middlewares/checkForSession.js
@@ -0,0 +1,10 @@
+module.exports = {
+    checkForSession: function (req, res, next)  {
+        const { session } = req
+
+        if ( !session.user) {
+            session.user = {username: '', cart: [], total: 0 }
+        }
+        next()
+    }
+}
